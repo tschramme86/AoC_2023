594aef010389d4320d3abb573df1f75121909921
Torben Schramme <t.schramme@live.de>
2023-12-01 11:37:09 +0100

    Day 01

---
+++ Days/AoCChallengeBase.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC2023.Days
+{
+    internal abstract class AoCChallengeBase : AoCChallenge
+    {
+        public virtual int Day { get; }
+        public virtual string Name { get; } = string.Empty;
+
+        protected virtual object? ExpectedTestResultPartOne { get; } = null;
+        protected virtual object? ExpectedTestResultPartTwo { get; } = null;
+        protected virtual bool ExtraTestDataPartTwo { get; } = false;
+
+        protected virtual string[] GetTestDataPartOne()
+        {
+            return System.IO.File.ReadAllLines($"Days/Day{Day:D2}/d{Day:D2}_input_test.txt");
+        }
+
+        protected virtual string[] GetTestDataPartTwo()
+        {
+            if(this.ExtraTestDataPartTwo)
+            {
+                return System.IO.File.ReadAllLines($"Days/Day{Day:D2}/d{Day:D2}_input_test2.txt");
+            }
+            return System.IO.File.ReadAllLines($"Days/Day{Day:D2}/d{Day:D2}_input_test.txt");
+        }
+
+        protected virtual string[] GetInputData()
+        {
+            return System.IO.File.ReadAllLines($"Days/Day{Day:D2}/d{Day:D2}_input.txt");
+        }
+
+        public bool TestPartOne()
+        {
+            var testData = GetTestDataPartOne();
+            var result = SolvePartOneInternal(testData);
+            if(!object.Equals(result, this.ExpectedTestResultPartOne))
+            {
+                Console.WriteLine($"Day {Day:D2}: Test data failed for Part One (expected {this.ExpectedTestResultPartOne} but got {result})");
+                return false;
+            }
+            Console.WriteLine($"Day {Day:D2}: Test data passed for Part One, result = {result}");
+            return true;
+        }
+
+        public bool TestPartTwo()
+        {
+            var testData = GetTestDataPartTwo();
+            var result = SolvePartTwoInternal(testData);
+            if(!object.Equals(result, this.ExpectedTestResultPartTwo))
+            {
+                Console.WriteLine($"Day {Day:D2}: Test data failed for Part Two (expected {this.ExpectedTestResultPartTwo} but got {result})");
+                return false;
+            }
+            Console.WriteLine($"Day {Day:D2}: Test data passed for Part Two, result = {result}");
+            return true;
+        }
+
+        public void SolvePartOne()
+        {
+            var inputData = GetInputData();
+            var result = SolvePartOneInternal(inputData);
+            Console.WriteLine($"Day {Day:D2}: Part One result = {result}");
+        }
+
+        public void SolvePartTwo()
+        {
+            var inputData = GetInputData();
+            var result = SolvePartTwoInternal(inputData);
+            Console.WriteLine($"Day {Day:D2}: Part Two result = {result}");
+        }
+
+        protected virtual object SolvePartOneInternal(string[] inputData)
+        {
+            return -1;
+        }
+
+        protected virtual object SolvePartTwoInternal(string[] inputData)
+        {
+            return -1;
+        }
+    }
+}
---
+++ Days/Day01/Day01Challenge.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC2023.Days.Day01
+{
+    internal class Day01Challenge : AoCChallengeBase
+    {
+        public override int Day => 1;
+        public override string Name => "Trebuchet?!";
+
+        protected override object ExpectedTestResultPartOne => 142;
+        protected override object ExpectedTestResultPartTwo => 281;
+        protected override bool ExtraTestDataPartTwo => true;
+
+        protected override object SolvePartOneInternal(string[] inputData)
+        {
+            var sum = 0;
+            foreach(var line in inputData)
+            {
+                sum += this.SumForLine(line);
+            }
+            return sum;
+        }
+
+        protected override object SolvePartTwoInternal(string[] inputData)
+        {
+            var sum = 0;
+            var replaceWordByDigit = new Dictionary<string, int>
+            {
+                { "one", 1 }, { "1", 1 },
+                { "two", 2 }, { "2", 2 },
+                { "three", 3 }, { "3", 3 },
+                { "four", 4 }, { "4", 4 },
+                { "five", 5 }, { "5", 5 },
+                { "six", 6 }, { "6", 6 },
+                { "seven", 7 }, { "7", 7 },
+                { "eight", 8 }, { "8", 8 },
+                { "nine", 9 }, { "9", 9 },
+            };
+            foreach(var line in inputData)
+            {
+                var newLine = line;
+
+                var firstDigit = 0;
+                var match = System.Text.RegularExpressions.Regex.Match(newLine, @"one|1|two|2|three|3|four|4|five|5|six|6|seven|7|eight|8|nine|9");
+                if(match.Success)
+                {
+                    firstDigit = replaceWordByDigit[match.Value];
+                }
+                match = System.Text.RegularExpressions.Regex.Match(newLine, @"one|1|two|2|three|3|four|4|five|5|six|6|seven|7|eight|8|nine|9", System.Text.RegularExpressions.RegexOptions.RightToLeft);
+                var lastDigit = 0;
+                if(match.Success)
+                {
+                    lastDigit = replaceWordByDigit[match.Value];
+                }
+
+                var number = firstDigit * 10 + lastDigit;
+                sum += number;
+            }
+            return sum;
+        }
+
+        private int SumForLine(string line)
+        {
+            var firstDigit = 0;
+            var lastDigit = 0;
+            foreach (var c in line)
+            {
+                // c is digit?
+                if (c >= '0' && c <= '9')
+                {
+                    firstDigit = c - '0';
+                    break;
+                }
+            }
+            foreach (var c in line.Reverse())
+            {
+                // c is digit?
+                if (c >= '0' && c <= '9')
+                {
+                    lastDigit = c - '0';
+                    break;
+                }
+            }
+            var number = firstDigit * 10 + lastDigit;
+            return number;
+        }
+    }
+}
